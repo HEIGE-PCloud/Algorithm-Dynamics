00d287dae69fa1bd98ac08b59da13728de0a040b
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-06-20 12:48:47 +0200

    fix(github-actions): fix build graph script

--- report/graphs/build_graphs.py
+++ report/graphs/build_graphs.py
@@ -1,10 +1,26 @@
 import os
+import argparse
 
-directory = './report/graphs'
+parser = argparse.ArgumentParser(description='Build graphs')
+parser.add_argument('--command',
+                    dest='command',
+                    default='./node_modules/.bin/mmdc',
+                    type=str,
+                    help='Command for mermaid-cli')
+parser.add_argument('--directory',
+                    dest='directory',
+                    default='./report/graphs',
+                    type=str,
+                    help='Directory of the graphs')
+args = parser.parse_args()
+
+directory = args.directory
+command = args.command
 files = os.listdir(directory)
 for file in files:
     if file[-4:] == '.mmd':
         filename = file[:-4]
         input_file = f'{directory}/{filename}.mmd'
         output_file = f'{directory}/{filename}.png'
-        os.system(f'mmdc -i {input_file} -o {output_file}')
+        print(f'{command} -i {input_file} -o {output_file}')
+        os.system(f'{command} -i {input_file} -o {output_file}')
