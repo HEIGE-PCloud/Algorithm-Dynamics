8cf8b6e00209fe3218d567b66b3b0725e9c98ac4
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-11-04 15:07:52 +0000

    report: add database design

--- src/Algorithm Dynamics.Core/Models/ProblemList.cs
+++ src/Algorithm Dynamics.Core/Models/ProblemList.cs
@@ -11,6 +11,5 @@ namespace Algorithm_Dynamics.Core.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Tag> Tags { get; set; }
     }
 }
