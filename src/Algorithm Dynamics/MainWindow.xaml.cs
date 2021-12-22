6c00fa9d2ac6842f5fe487469a2a625f38df22b9
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-12-22 13:34:24 +0000

    dev: prepare an empty project for development

--- src/Algorithm Dynamics/MainWindow.xaml.cs
+++ src/Algorithm Dynamics/MainWindow.xaml.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Runtime.Versioning;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -12,38 +21,16 @@ namespace Algorithm_Dynamics
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    [SupportedOSPlatform("windows10.0.10240.0")]
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-            Title = "Algorithm Dynamics";
-            ExtendsContentIntoTitleBar = false;
-            MainNavigationView.SelectedItem = MainNavigationView.MenuItems.OfType<NavigationViewItem>().First();
         }
-        private void MainNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            if (args.IsSettingsSelected)
-            {
-                contentFrame.Navigate(typeof(Pages.SettingsPage));
-            }
-            else
-            {
-                NavigationViewItem selectedItem = args.SelectedItem as NavigationViewItem;
-                if (selectedItem != null)
-                {
-                    string selectedItemTag = selectedItem.Tag as string;
-                    //sender.Header = "Sample Page " + selectedItemTag.Substring(selectedItemTag.Length - 1);
-                    string pageName = "Algorithm_Dynamics.Pages." + selectedItemTag;
-                    Type pageType = Type.GetType(pageName);
-                    contentFrame.Navigate(pageType);
-                }
-            }
+            myButton.Content = "Clicked";
         }
-        public void SelectItem(NavigationViewItem item) => MainNavigationView.SelectedItem = item;
-        public void SelectMenuItemIndex(int index) => SelectItem((NavigationViewItem)MainNavigationView.MenuItems[index]);
-        public void SelectFooterItemIndex(int index) => SelectItem((NavigationViewItem)MainNavigationView.FooterMenuItems[index]);
-        public void SelectSettingItem() => MainNavigationView.SelectedItem = MainNavigationView.SettingsItem;
     }
 }
