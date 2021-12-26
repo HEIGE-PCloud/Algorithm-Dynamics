d63d0f23b6162464a068473b500806bc63f5f52c
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-12-26 11:13:10 +0000

    dev: create the NavigationView

---
+++ src/Algorithm Dynamics/Pages/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Algorithm_Dynamics.Pages
+{
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
