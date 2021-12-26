5abf33fa895a03841eb0d6ffa6e3c733014f1aa5
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-12-26 17:49:44 +0000

    dev: finish coding for the HomePage

---
+++ src/Algorithm Dynamics/Models/HomePageGridViewItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Algorithm_Dynamics.Models
+{
+    public class HomePageGridViewItem
+    {
+        public string Name { get; set; }
+        public Symbol Icon { get; set; }
+        public HomePageGridViewItem(string name, Symbol icon)
+        {
+            Name = name;
+            Icon = icon;
+        }
+    }
+}
