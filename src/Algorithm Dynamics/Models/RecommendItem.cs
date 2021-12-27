e0ed6d4a1748b0b15c6950a1f34991be45547295
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-12-27 11:15:42 +0000

    dev: optimize homepage looking

---
+++ src/Algorithm Dynamics/Models/RecommendItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Dynamics.Models
+{
+    public class RecommendItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public RecommendItem(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
