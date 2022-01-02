7d915e4f14a25f51e683b653124ee29963b734ae
PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-01-02 20:40:44 +0000

    dev: import monaco editor and process resize event
    Signed-off-by: PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>

---
+++ src/Algorithm Dynamics/Controls/CodeEditor.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using Windows.ApplicationModel;
+using Windows.Storage;
+
+namespace Algorithm_Dynamics.Controls
+{
+    public sealed partial class CodeEditor : UserControl
+    {
+        public CodeEditor()
+        {
+            InitializeComponent();
+            InitializeWebViewAsync();
+        }
+
+        /// <summary>
+        /// Initialize the WebView control and load Editor.html
+        /// </summary>
+        async void InitializeWebViewAsync()
+        {
+            // Ensure the CoreWebView2 is loaded
+            await WebView.EnsureCoreWebView2Async();
+
+            // Set the mapping
+            StorageFolder AssetsDirectory = await Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+            WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "localeditor.algorithmdynamics.com",
+                AssetsDirectory.Path,
+                CoreWebView2HostResourceAccessKind.Allow
+            );
+
+            // Load Editor.html
+            WebView.Source = new Uri("http://localeditor.algorithmdynamics.com/Editor.html");
+        }
+    }
+}
