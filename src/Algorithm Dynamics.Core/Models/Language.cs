2e9d850ebf174376348dfc5dbc4d99cf32422eeb
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-01-09 20:47:43 +0000

    dev: connect Judger with Playground

--- src/Algorithm Dynamics.Core/Models/Language.cs
+++ src/Algorithm Dynamics.Core/Models/Language.cs
@@ -2,14 +2,16 @@ namespace Algorithm_Dynamics.Core.Models
 {
     public class Language
     {
+        public string DisplayName { get; set; }
         public string Name { get; set; }
         public bool NeedCompile { get; set; }
         public string CompileCommand { get; set; }
         public string CompileArguments { get; set; }
         public string RunCommand { get; set; }
         public string RunArguments { get; set; }
-        public Language(string name, bool needCompile, string compileCommand, string compileArguments, string runCommand, string runArguments)
+        public Language(string displayName, string name, bool needCompile, string compileCommand, string compileArguments, string runCommand, string runArguments)
         {
+            DisplayName = displayName;
             Name = name;
             NeedCompile = needCompile;
             CompileCommand = compileCommand;
@@ -17,8 +19,9 @@ namespace Algorithm_Dynamics.Core.Models
             RunCommand = runCommand;
             RunArguments = runArguments;
         }
-        public Language(string name, string runCommand, string runArguments)
+        public Language(string displayName, string name, string runCommand, string runArguments)
         {
+            DisplayName = displayName;
             Name = name;
             NeedCompile = false;
             RunCommand = runCommand;
@@ -34,15 +37,47 @@ namespace Algorithm_Dynamics.Core.Models
         public readonly static Language Python = new(
             "Python",
             "python",
+            "python",
             "{SourceCodeFilePath}"
+        );
+        public readonly static Language C = new(
+            "C",
+            "c",
+            true,
+            "gcc",
+            "-x c {SourceCodeFilePath} -o {ExecutableFilePath}",
+            "{ExecutableFilePath}",
+            ""
         );
         public readonly static Language Cpp = new(
             "C++",
+            "cpp",
             true,
             "g++",
             "-x c++ {SourceCodeFilePath} -o {ExecutableFilePath}",
             "{ExecutableFilePath}",
             ""
+        );
+        public readonly static Language Rust = new(
+            "Rust",
+            "rust",
+            true,
+            "rustc",
+            "{SourceCodeFilePath} -o {ExecutableFilePath}",
+            "{ExecutableFilePath}",
+            ""
+        );
+        public readonly static Language JavaScript = new(
+            "JavaScript",
+            "javascript",
+            "node",
+            "{SourceCodeFilePath}"
+        );
+        public readonly static Language Java = new(
+            "Java",
+            "java",
+            "java",
+            "{SourceCodeFilePath}"
         );
     }
 }
