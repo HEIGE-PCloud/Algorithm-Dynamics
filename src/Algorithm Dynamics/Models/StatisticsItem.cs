19b67568398db010feb9c27624f8dc05f6996bd6
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-02 09:20:47 +0000

    feat: implement the account page
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

---
+++ src/Algorithm Dynamics/Models/StatisticsItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Dynamics.Models
+{
+    public class StatisticsItem
+    {
+        public StatisticsItem(string title, string data)
+        {
+            Title = title;
+            Data = data;
+        }
+
+        public string Title { get; set; }
+        public string Data { get; set; }
+    }
+}
