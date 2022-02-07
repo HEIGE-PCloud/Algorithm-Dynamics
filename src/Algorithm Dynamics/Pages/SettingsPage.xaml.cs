946e670fd9ff2e6f41a88af04091faa5c0cf401d
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-07 15:16:50 +0000

    dev: add settings page and assignment details page

--- src/Algorithm Dynamics/Pages/SettingsPage.xaml.cs
+++ src/Algorithm Dynamics/Pages/SettingsPage.xaml.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -26,6 +27,50 @@ namespace Algorithm_Dynamics.Pages
         public SettingsPage()
         {
             this.InitializeComponent();
+
+            GetCurrentTheme();
+        }
+
+        private async void AddLangButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog();
+            dialog.Title = "Add a new Programming Language Configuration";
+            dialog.PrimaryButtonText = "Add";
+            dialog.CloseButtonText = "Cancel";
+            dialog.Content = "Content";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.XamlRoot = this.Content.XamlRoot;
+            var result = await dialog.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                // TODO: Delete the selected problem list
+                throw new NotImplementedException();
+            }
+        }
+        private void GetCurrentTheme()
+        {
+            if (App.m_window.Content is FrameworkElement rootElement)
+            {
+                var currentTheme = rootElement.RequestedTheme.ToString();
+                ThemePanel.Children.Cast<RadioButton>().FirstOrDefault(c => c?.Tag?.ToString() == currentTheme).IsChecked = true;
+            }
+        }
+        private void ThemeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var selectedTheme = ((RadioButton)sender)?.Tag?.ToString();
+
+            if (selectedTheme != null && App.m_window.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = GetEnum<ElementTheme>(selectedTheme);
+            }
+        }
+        private static TEnum GetEnum<TEnum>(string text) where TEnum : struct
+        {
+            if (!typeof(TEnum).GetTypeInfo().IsEnum)
+            {
+                throw new InvalidOperationException("Generic parameter 'TEnum' must be an enum.");
+            }
+            return (TEnum)Enum.Parse(typeof(TEnum), text);
         }
     }
 }
