ede68310b11598e019835758dbceb9554d4ac3a2
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-21 23:09:15 +0000

    dev: create simple DataAccess class and unit test
    Follow the instruction from https://docs.microsoft.com/en-us/windows/uwp/data-access/sqlite-databases#choose-an-abstraction-layer to create a simple DataAccess class as a template for later development

---
+++ src/Algorithm Dynamics.Core/Helpers/DataAccess.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Algorithm_Dynamics.Core.Helpers
+{
+    public static class DataAccess
+    {
+        private static string DbPath;
+        public static void InitializeDatabase(string dbPath)
+        {
+            DbPath = dbPath;
+            File.CreateText(dbPath).Close();
+            using (SqliteConnection db = new SqliteConnection($"Filename={DbPath}"))
+            {
+                db.Open();
+
+                string tableCommand = 
+                    @"CREATE TABLE IF NOT EXISTS
+                    MyTable (
+                        Primary_Key INTEGER PRIMARY KEY,
+                        Text_Entry TEXT
+                    )";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+                db.Close();
+            }
+        }
+
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={DbPath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+
+        public static List<string> GetData()
+        {
+            List<string> entries = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection($"Filename={DbPath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+        }
+
+
+    }
+}
---
+++ src/Algorithm Dynamics.Test/TestDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm_Dynamics.Core.Helpers;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Algorithm_Dynamics.Test
+{
+    [TestClass]
+    public class TestDataAccess
+    {
+        [TestMethod]
+        public void TestSingleData()
+        {
+            DataAccess.InitializeDatabase(@"temp\TestSingleData.db");
+            DataAccess.AddData("Text1");
+            Assert.AreEqual(DataAccess.GetData().Count, 1);
+            Assert.AreEqual(DataAccess.GetData()[0], "Text1");
+        }
+        [TestMethod]
+        public void TestMultipleData()
+        {
+            DataAccess.InitializeDatabase(@"temp\TestMultipleData.db");
+            int count = 100;
+            List<string> list = new();
+            for (int i = 0; i < count; i++)
+            {
+                DataAccess.AddData($"Text{count}");
+                list.Add($"Text{count}");
+            }
+            Assert.AreEqual(DataAccess.GetData().Count, count);
+            CollectionAssert.AreEqual(list, DataAccess.GetData());
+
+        }
+    }
+}
