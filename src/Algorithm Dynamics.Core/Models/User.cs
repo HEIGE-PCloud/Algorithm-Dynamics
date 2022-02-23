4717f6253c7a0f320a037644c0d64c9df4afe606
PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-02-23 18:13:15 +0000

    Dev/database (#63)
    * dev: create simple DataAccess class and unit test
    
    Follow the instruction from https://docs.microsoft.com/en-us/windows/uwp/data-access/sqlite-databases#choose-an-abstraction-layer to create a simple DataAccess class as a template for later development
    
    * dev: add User table
    
    Create user table if not exists when init. Create AddUser and GetAllUsers function. Add tests for these functions.

--- src/Algorithm Dynamics.Core/Models/User.cs
+++ src/Algorithm Dynamics.Core/Models/User.cs
@@ -12,12 +12,29 @@ namespace Algorithm_Dynamics.Core.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
-        public User(string name, string email, Role role)
+        private User(Guid uid, string name, string email, Role role)
         {
-            Uid = Guid.NewGuid();
+            Uid = uid;
             Name = name;
             Email = email;
             Role = role;
+        }
+        public static User Create(string name, string email, Role role)
+        {
+            return new User(Guid.NewGuid(), name, email, role);
+        }
+        public static User Load(Guid uid, string name, string email, Role role)
+        {
+            return new User(uid, name, email, role);
+        }
+        public override bool Equals(object obj)
+        {
+            var user = obj as User;
+
+            if (user == null)
+                return false;
+
+            return user.Uid == Uid && user.Name == Name && user.Email == Email && user.Role == Role;
         }
     }
     public enum Role
---
+++ src/Algorithm Dynamics.Test/TestDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm_Dynamics.Core.Helpers;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Algorithm_Dynamics.Core.Models;
+
+namespace Algorithm_Dynamics.Test
+{
+    [TestClass]
+    public class TestDataAccess
+    {
+        [TestMethod]
+        public void TestSingleData()
+        {
+            DataAccess.InitializeDatabase(@"temp\TestSingleData.db");
+            DataAccess.AddData("Text1");
+            Assert.AreEqual(DataAccess.GetData().Count, 1);
+            Assert.AreEqual(DataAccess.GetData()[0], "Text1");
+        }
+
+        [TestMethod]
+        public void TestMultipleData()
+        {
+            DataAccess.InitializeDatabase(@"temp\TestMultipleData.db");
+            int count = 100;
+            List<string> list = new();
+            for (int i = 0; i < count; i++)
+            {
+                DataAccess.AddData($"Text{count}");
+                list.Add($"Text{count}");
+            }
+            Assert.AreEqual(DataAccess.GetData().Count, count);
+            CollectionAssert.AreEqual(list, DataAccess.GetData());
+        }
+
+        [TestMethod]
+        public void TestUserData()
+        {
+            User user = User.Create("Test User", "test@example.com", Role.Student);
+            DataAccess.InitializeDatabase(@"temp\UserData.db");
+            DataAccess.AddUser(user);
+
+            List<User> expectedUsers = new() { user };
+            List<User> actualUsers = DataAccess.GetAllUsers();
+            Assert.AreEqual(actualUsers.Count, 1);
+            CollectionAssert.AreEqual(actualUsers, expectedUsers);
+        }
+    }
+}
