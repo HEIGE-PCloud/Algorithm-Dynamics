3fb77907ed8578834efeb4e6461c59ce140c2a75
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-27 22:52:39 +0000

    chore: run code clean up

---
+++ src/Algorithm Dynamics/Helpers/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Algorithm_Dynamics.Helpers
+{
+    internal static class FileHelper
+    {
+        internal async static Task<StorageFile> FileOpenPicker(string fileTypeFilter = "*")
+        {
+            // https://github.com/microsoft/WindowsAppSDK/issues/1188
+            // https://docs.microsoft.com/en-us/windows/uwp/files/quickstart-using-file-and-folder-pickers
+            var filePicker = new FileOpenPicker();
+
+            // Get the current window's HWND by passing in the Window object
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.m_window);
+
+            // Associate the HWND with the file picker
+            WinRT.Interop.InitializeWithWindow.Initialize(filePicker, hwnd);
+
+            // Use file picker like normal!
+            filePicker.FileTypeFilter.Add(fileTypeFilter);
+            StorageFile file = await filePicker.PickSingleFileAsync();
+            return file;
+        }
+    }
+}
