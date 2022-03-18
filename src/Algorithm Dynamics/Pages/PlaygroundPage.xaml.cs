333a86386313be89ed80eefdb564b51a0d375897
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-03-18 12:21:28 +0000

    report: add timelimit settings and memorylimit settings
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- src/Algorithm Dynamics/Pages/PlaygroundPage.xaml.cs
+++ src/Algorithm Dynamics/Pages/PlaygroundPage.xaml.cs
@@ -14,20 +14,29 @@ namespace Algorithm_Dynamics.Pages
             InitializeComponent();
             Core.Models.Language.All.ForEach(lang => Languages.Add(lang));
         }
-        public string Code { get; set; }
-        public string Input { get; set; }
-        private const int DEFAULT_RUN_CODE_TIMELIMIT = 1000;
-        private const int DEFAULT_RUN_CODE_MEMORYLIMIT = 64 * 1024 * 1024;
+
+        private string Code;
+        private string Input;
+        private const int ms = 1000;
+        private const int MB = 1024 * 1024;
+        private const int DEFAULT_RUN_CODE_TIMELIMIT = 1 * ms;
+        private const int DEFAULT_RUN_CODE_MEMORYLIMIT = 64 * MB;
         private const string TIMELIMIT_KEY = "RunCodeTimeLimit";
         private const string MEMORYLIMIT_KEY = "RunCodeMemoryLimit";
         private int _timeLimit = DEFAULT_RUN_CODE_TIMELIMIT;
         private int _memoryLimit = DEFAULT_RUN_CODE_MEMORYLIMIT;
-
         private readonly ObservableCollection<Language> Languages = new();
+        
+        /// <summary>
+        /// RunCode using <see cref="Judger.RunCode"/>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void RunCodeButton_Click(object sender, RoutedEventArgs e)
         {
-            // Read Run Code Time Limit and Memory Limit from the Setting
-            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            ApplicationDataContainer localSettings 
+                = ApplicationData.Current.LocalSettings;
+            // Read RunCode TimeLimit from settings
             var CurrentTimeLimit = localSettings.Values[TIMELIMIT_KEY];
             if (CurrentTimeLimit != null)
             {
@@ -35,8 +44,11 @@ namespace Algorithm_Dynamics.Pages
             }
             else
             {
-                localSettings.Values[TIMELIMIT_KEY] = DEFAULT_RUN_CODE_TIMELIMIT;
+                localSettings.Values[TIMELIMIT_KEY] 
+                    = DEFAULT_RUN_CODE_TIMELIMIT;
             }
+
+            // Read RunCode MemoryLimit from settings
             var CurrentMemoryLimit = localSettings.Values[MEMORYLIMIT_KEY];
             if (CurrentMemoryLimit != null)
             {
@@ -44,22 +56,36 @@ namespace Algorithm_Dynamics.Pages
             }
             else
             {
-                localSettings.Values[MEMORYLIMIT_KEY] = DEFAULT_RUN_CODE_MEMORYLIMIT;
+                localSettings.Values[MEMORYLIMIT_KEY] 
+                    = DEFAULT_RUN_CODE_MEMORYLIMIT;
             }
 
-            var progress = new Progress<int>(percent => { RunCodeProgressBar.Value = percent; });
-
+            // Set the progress bar and button
+            var progress = new Progress<int>(percent 
+                => { RunCodeProgressBar.Value = percent; });
             RunCodeButton.IsEnabled = false;
 
-            RunCodeResult result = await Judger.RunCode(CodeEditor.Code, Input, Languages[LanguageComboBox.SelectedIndex], _timeLimit, _memoryLimit, progress);
+            // Run Code
+            RunCodeResult result = await Judger.RunCode(
+                CodeEditor.Code,
+                Input,
+                Languages[LanguageComboBox.SelectedIndex],
+                _timeLimit,
+                _memoryLimit,
+                progress);
 
+            // Restore the button and display result
             RunCodeButton.IsEnabled = true;
-
-            StatusTextBlock.Text = $"{result.Result} Time: {result.CPUTime} ms Memory: {result.MemoryUsage / 1024 / 1024} MB";
+            StatusTextBlock.Text 
+                = $"{result.Result} Time: {result.CPUTime} ms Memory: {result.MemoryUsage / MB} MB";
             OutputBox.Text = result.StandardOutput + result.StandardError;
-
         }
 
+        /// <summary>
+        /// Set CodeEditor language based on the selected language config
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void LanguageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Language language = LanguageComboBox.SelectedItem as Language;
